90f6ee0af274033a25cbe61dab77cb20461a13b5
Elaine Holanda <elaine.holandac@gmail.com>
2025-06-12 09:23:15 -0300

    Update guloso_sbpo.cpp

--- guloso_sbpo.cpp
+++ guloso_sbpo.cpp
@@ -2,73 +2,86 @@
 using namespace std;
 
 struct Pedido{
-	vector<int> itens;
-	int id, qtd;
+	vector<pair<int, int>> itens;
+	int id;		//indice do pedido/corredor
+	int qtd;	//quantidade total de itens
+	bool usado; //se o pedido/corredor ja foi selecionado
 };
 typedef Pedido Corredor;
 
 vector<Pedido> pedidos;
 vector<Corredor> corredores;
-
-set<int> corredores_wave;
 vector<int> pedidos_wave;
+vector<int> corredores_wave;
 
 int LB, UB;     // limites
 int N, K, M; 	// N #pedidos, K #itens, M #corredores
-int total;		// total de itens selecionados na wave
+int total=0;		// total de itens selecionados na wave
 
-bool ordena(Pedido A, Pedido B){
-	return A.qtd>B.qtd;
-}
+bool ordena(Pedido A, Pedido B){return A.qtd>B.qtd;}
 
 bool pedido_viavel(Pedido p, Corredor c){ // checa se um corredor c pode atender um pedido p
 
-	if(total+p.qtd+c.qtd>UB) return false;
+	if(total+p.qtd>UB) return false;
+	if(p.usado) return false;
 
 	for(int i=0; i<p.itens.size(); i++){
-		if(p.itens[i]>c.itens[i]) return false;
+		bool item_achado=false;
+		for(int j=0; j<c.itens.size(); j++){
+			if(p.itens[i].first == c.itens[j].first){
+				item_achado = true;
+				if(p.itens[i].second>c.itens[j].second) return false;
+			}
+		}
+		if(!item_achado) return false;
 	}
 	return true;
 }
 
 void remove_itens(Pedido p, Corredor c){ // subtrai do corredor c os itens do pedido p
 	for(int i=0; i<p.itens.size(); i++){
-		c.itens[i]-=p.itens[i];
+		for(int j=0; j<c.itens.size(); j++){
+			if(p.itens[i].first==c.itens[j].first){
+				c.itens[j].second-=p.itens[i].second;
+				c.qtd-=p.itens[i].second;
+			}
+		}
 	}
 }
 
 void seleciona_pedidos(){
 
-	int p_atual=0; // pedido atual
-	int c_atual=0; // corredor atual
+	sort(pedidos.begin(), pedidos.end(), ordena);			//ordena pedidos
 
-	while(!pedidos.empty() and total<LB){ //procura maximizar uma wave viavel
+	while(total<=UB){ //procura maximizar uma wave viavel
 
-		cout<<"p_atual = "<<p_atual<<", c_atual = "<<c_atual<<"\n";
+		sort(corredores.begin(), corredores.end(), ordena); 	//ordena corredores
 
-		cout<<"fila atual de pedidos: ";
-		for(int i=0; i<pedidos.size(); i++) cout<<pedidos[i].id<<" ";
-		cout<<"\n";
-		
-		if(pedido_viavel(pedidos[p_atual], corredores[c_atual])){
+		int ok=false;
 
-			remove_itens(pedidos[p_atual], corredores[c_atual]);
-			total+=pedidos[p_atual].qtd;
+		for(int j=0; j<corredores.size(); j++){
+			for(int i=0; i<pedidos.size(); i++){
 
-			pedidos_wave.push_back(pedidos[p_atual].id);
-			corredores_wave.insert(corredores[c_atual].id);
+				if(pedido_viavel(pedidos[i], corredores[j])){
 
-			cout<<"o pedido selecionado foi: "<<pedidos[p_atual].id<<"\n";
-			cout<<"quantidade de itens: "<<total<<"\n";
+					remove_itens(pedidos[i], corredores[j]);
+					total+=pedidos[i].qtd;
+					pedidos[i].qtd=-1;
 
-			pedidos.erase(pedidos.begin()+p_atual);
+					pedidos_wave.push_back(pedidos[i].id);
+					if(!corredores[j].usado) corredores_wave.push_back(corredores[j].id);
 
-		}else p_atual++;
-
-		if(p_atual==pedidos.size()){
-			if(c_atual==corredores.size()) return;
-			p_atual=0;
-			c_atual++;
+					cout<<"o pedido "<<pedidos[i].id<<" foi retirado do corredor "<<corredores[j].id<<" ["<<j<<"]"<<"\n";
+					
+					ok=true;
+					pedidos[i].usado=true;
+					corredores[j].usado=true;
+				}
+			}
+		}
+		if(ok==false){
+			cout<<"NENHUM NOVO PEDIDO VIAVEL\n";
+			return;	
 		}
 	}
 }
@@ -76,6 +89,8 @@ void seleciona_pedidos(){
 int main(){
 
 	ifstream input("entrada.txt");
+	//ifstream input("entrada2.txt");
+	//ifstream input("instance_0001.txt");
 	ofstream output("saida.txt");
 
 	input>>N>>K>>M;
@@ -84,13 +99,12 @@ int main(){
 	for(int i=0; i<N; i++){
 		int x;
 		input>>x;
-		pedidos.push_back(Pedido{{}, i, 0});
-		pedidos[i].itens.resize(K);
+		pedidos.push_back(Pedido{{}, i, 0, false});
 
 		for(int j=0; j<x; j++){
 			int a, b;
 			input>>a>>b;
-			pedidos[i].itens[a]=b;
+			pedidos[i].itens.push_back({a, b});
 			pedidos[i].qtd+=b;
 		}
 	}
@@ -98,28 +112,25 @@ int main(){
 	for(int i=0; i<M; i++){
 		int x;
 		input>>x;
-		corredores.push_back(Corredor{{}, i, 0});
-		corredores[i].itens.resize(K);
+		corredores.push_back(Corredor{{}, i, 0, false});
 
 		for(int j=0; j<x; j++){
 			int a, b;
 			input>>a>>b;
-			corredores[i].itens[a]=b;
+			corredores[i].itens.push_back({a, b});
 			corredores[i].qtd+=b;
 		}
 	}
 
-	input>>LB>>UB; // le limites inferior e superior
-
-	sort(pedidos.begin(), pedidos.end(), ordena);			//ordena pedidos
-	sort(corredores.begin(), corredores.end(), ordena); 	//ordena corredores
+	input>>LB>>UB; // le limites inferior e superior	
 
 	seleciona_pedidos();
+	cout<<"quantidade de itens: "<<total<<"\n";
 
 	output<<pedidos_wave.size()<<"\n";
 	for(int i=0; i<pedidos_wave.size(); i++) output<<pedidos_wave[i]<<"\n";
 	output<<corredores_wave.size()<<"\n";
-	for(set<int>::iterator it=corredores_wave.begin(); it!=corredores_wave.end(); it++) output<<*it<<"\n";
+	for(int i=0; i<corredores_wave.size(); i++) output<<corredores_wave[i]<<"\n";
 
 	return 0;
 }
